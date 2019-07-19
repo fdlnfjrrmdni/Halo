268dfd69355f4094be9f7b02827c2b89af86b9cb
fdlnfjrrmdni <fdlnfjrrmdni@gmail.com>
2019-07-19 21:24:26 +0700

    add map

--- src/screens/Maps.js
+++ src/screens/Maps.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, StatusBar, Image } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, StatusBar, Image, Dimensions } from 'react-native';
 import Header from '../component/Header';
 import { colors, fonts, padding } from './style';
 // import firebase from 'firebase';
@@ -10,11 +11,37 @@ export default class Maps extends Component {
 			<View>
 				<StatusBar backgroundColor="transparent" barStyle="dark-content" />
 				<Header 
-					title='Maps'
+					title='Map'
 					leftIcon={require('../assets/icons/left.png')}
 					leftPress={() => this.props.navigation.goBack()}
 				/>
+				<View style={styles.container}>
+					<MapView
+						provider ={PROVIDER_GOOGLE} // remove if not using Google Maps
+						style    ={styles.map}
+						region   ={{
+							latitude      : 37.78825,
+							longitude     : -122.4324,
+							latitudeDelta : 0.015,
+							longitudeDelta: 0.0121,
+				       	}}
+				    >
+				    </MapView>
+			    </View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+ 	container: {
+   		...StyleSheet.absoluteFillObject,
+   		height: Dimensions.get('window').height,
+   		width: Dimensions.get('window').width,
+   		justifyContent: 'flex-end',
+   		alignItems: 'center',
+ 	},
+ 	map: {
+   		...StyleSheet.absoluteFillObject,
+ 	},
+});
